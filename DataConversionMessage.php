e20ea2da54a149de8f6fb821686246f3254f6084
Rangel Reale <rangelspam@gmail.com>
2016-12-08 16:08:14 -0200

    * Initial import

---
+++ DataConversionMessage.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace RangelReale\yii2mdh;
+
+class DataConversionMessage extends RangelReale\mdh\DataConversionMessage
+{
+    public function getAttribute($options)
+    {
+        return 'Value';
+    }
+
+    public function getMessage($datatype, $parseOrFormat, $value, $options, $extra)
+    {
+        $attribute = $this->getAttribute($options);
+        
+        if ($datatype == 'integer') {
+            return \Yii::t('yii', '{attribute} must be an integer.', [
+                'attribute' => $attribute,
+            ]);
+        } elseif (substr($datatype, 0, 7)  == 'decimal') {
+            return \Yii::t('yii', '{attribute} must be a number.', [
+                'attribute' => $attribute,
+            ]);
+        } elseif ($datatype == 'date' || $datatype == 'time' || $datatype == 'datetime') {
+            return \Yii::t('yii', 'The format of {attribute} is invalid.', [
+                'attribute' => $attribute,
+            ]);
+        }
+        return parent::getMessage($datatype, $parseOrFormat, $value, $options, $extra);
+    }
+}
---
+++ MDH.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace RangelReale\yii2mdh;
+
+use yii\base\Object;
+use RangelReale\mdh\BaseMDH;
+
+class MDH extends Object
+{
+    public $mdh;
+    
+    public function init()
+    {
+        $this->mdh = new Yii2MDH();
+        $this->mdh->setDataConversionMessage(new DataConversionMessage());
+    }
+}
+
+class Yii2MDH extends BaseMDH
+{
+    public function getLocale()
+    {
+        return \Yii::$app->language;
+    }
+}
